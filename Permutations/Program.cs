e02b7fa0779acb87beac6852c3dee45cd3443ca1
jd_786 <jaydeep_shil@certree.com>
2022-05-03 21:31:25 +0530

    Add project files.

---
+++ Permutations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      var nums = new int[] { 1, 2, 3 };
+      p.Permute(nums);
+    }
+
+    public IList<IList<int>> Permute(int[] nums)
+    {
+      var result = new List<IList<int>>();
+      Backtracking(nums, result, new HashSet<int>());
+      return result;
+    }
+
+    private void Backtracking(int[] nums, List<IList<int>> result, HashSet<int> temp)
+    {
+      if (temp.Count == nums.Length)
+      {
+        result.Add(new List<int>(temp));
+      }
+      else
+      {
+        for (int i = 0; i < nums.Length; i++)
+        {
+          if (temp.Contains(nums[i])) continue; // All the game change happened in this line only
+          temp.Add(nums[i]);
+          Backtracking(nums, result, temp);
+          var list = temp.ToList();
+          temp.Remove(list.ElementAt(temp.Count - 1));
+        }
+      }
+    }
+  }
+}
